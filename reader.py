f7882eb4f3c41b236a861a10bdd864d1e2fd41e1
Rogowski Mirosław <mirek.rogowski@hotmail.com>
2021-12-22 13:22:02 +0100

    Update reader.py
    open file
    check file

--- reader.py
+++ reader.py
@@ -13,3 +13,17 @@ def list_directory(file, path):
         #     print(f"DIR - > {line}")
         print(f"file -> {line}") if os.path.isfile(line) else print(f"DIR - > {line}")
     exit()
+
+
+def open_file(file):
+    with open(file, "r", newline="\n") as fr_csv:
+        for line in csv.reader(fr_csv):
+            file_csv_line.append(line)
+
+
+def check_file(file, directory):
+    # print("check diretory",directory)
+    # print("check file ",file)
+    file_path = directory+ '\\'+ file
+    # print("file_path", file_path)
+    open_file(file_path) if os.path.isfile(file) else list_directory(file, directory)
